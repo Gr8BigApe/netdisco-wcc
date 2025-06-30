2a508ae9a3121d4932459018534f8b8261f94c41
Gr8BigApe <137727436+Gr8BigApe@users.noreply.github.com>
2025-06-30 09:23:57 -0400

    Update PaloAlto.pm

--- custom_plugins/PaloAlto.pm
+++ custom_plugins/PaloAlto.pm
@@ -1,1 +1,26 @@
+package App::Netdisco::SSHCollector::Platform::PaloAlto;
+
+use strict;
+use warnings;
+use base 'App::Netdisco::SSHCollector::Platform::Base';
+
+sub arp {
+    my ($self, $ssh, @args) = @_;
+    my @output = $ssh->capture("show arp all");
+
+    return [] unless @output;
+
+    my @entries;
+    foreach my $line (@output) {
+        next unless $line =~ /^\s*(\d{1,3}(?:\.\d{1,3}){3})\s+([\da-f]{2}(?::[\da-f]{2}){5})\s+/i;
+        push @entries, {
+            ip  => $1,
+            mac => $2,
+        };
+    }
+
+    return \@entries;
+}
+
+1;
 
